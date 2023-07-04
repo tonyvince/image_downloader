b59efda717147d427786831c03c2df420952368f
Tony Thayil <tonyvince7@gmail.com>
2023-07-04 17:55:32 +0200

    Use threads to download images concurrently

--- lib/image_downloader.rb
+++ lib/image_downloader.rb
@@ -5,25 +5,29 @@ class ImageDownloader
   def self.download_images(urls, target_dir = "./images")
     Dir.mkdir(target_dir) unless File.exist?(target_dir)
 
-    urls.each do |url|
-      begin
-        uri = URI.parse(url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true if uri.scheme == 'https'
-        head_response = http.request_head(uri.path)
+    threads = urls.map do |url|
+      Thread.new do
+        begin
+          uri = URI.parse(url)
+          http = Net::HTTP.new(uri.host, uri.port)
+          http.use_ssl = true if uri.scheme == 'https'
+          head_response = http.request_head(uri.path)
 
-        if head_response["content-type"].start_with? "image/"
-          URI.open(url) do |u|
-            file_path = "#{target_dir}/#{File.basename(uri.path)}"
-            File.open(file_path, 'wb') { |f| f.write(u.read) }
-            puts "Downloaded: #{url}"
+          if head_response["content-type"].start_with? "image/"
+            URI.open(url) do |u|
+              file_path = "#{target_dir}/#{File.basename(uri.path)}"
+              File.open(file_path, 'wb') { |f| f.write(u.read) }
+              puts "Downloaded: #{url}"
+            end
+          else
+            puts "#{url} is not an image. Skipping."
           end
-        else
-          puts "#{url} is not an image. Skipping."
+        rescue => e
+          puts "Error downloading #{url}. Error message: #{e.message}"
         end
-      rescue => e
-        puts "Error downloading #{url}. Error message: #{e.message}"
       end
     end
+
+    threads.each(&:join) # Wait for all threads to finish
   end
 end
